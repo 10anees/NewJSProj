1fbb1111902131696256e538bea21f1fe194715e
Anees <aneeshamid123@gmail.com>
2025-03-09 07:21:23 -0400

    Scope lab commit

---
+++ Scope/scope_lab.js
@@ -0,0 +1,25 @@
+var globalVar = "Global Var";
+let globalLet = "Global Let";
+const globalConst = "Global const";
+
+{
+    var blockVar = "Block var";
+    let blockLet = "Block let";
+    const blockConst = "Block const";
+}
+
+// Global scope
+console.log(globalVar); // Output: "I'm a global variable"
+console.log(globalLet); // Output: "I'm also global, but scoped with let"
+console.log(globalConst); // Output: "I'm a global constant"
+
+function show() {
+    var functionVar = "I'm a block-scoped var";
+    let functionLet = "I'm a block-scoped let";
+    const functionConst = "I'm a block-scoped const";
+}
+show();
+
+console.log(functionVar); // Throws ReferenceError
+console.log(functionLet); // Throws ReferenceError
+console.log(functionConst); // Throws ReferenceError
